e9b632807329c677f466cb3b964efef5068ee37e
ccorwyn <alex@gorowara.(none)>
2013-03-19 14:41:44 -0400

    Added new classes for toolbar

---
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/toolbar/CreateRequirementAction.java
@@ -0,0 +1,41 @@
+/**
+ * Action invoked upon use of the Create Requirement key
+ * Heavily adapted from CreateDefectAction in the DefectTracker module
+ * 
+ * @author Alex Gorowara 
+ * 
+ */
+
+package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.toolbar;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
+
+import javax.swing.AbstractAction;
+
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.tabs.MainTabController;
+
+/**
+ * Action that calls {@link MainTabController#addCreateDefectTab()}, default mnemonic key is C. 
+ */
+@SuppressWarnings("serial")
+public class CreateRequirementAction extends AbstractAction {
+
+	private final MainTabController controller;
+	
+	/**
+	 * Create a CreateDefectAction
+	 * @param controller When the action is performed, controller.addCreateDefectTab() is called
+	 */
+	public CreateRequirementAction(MainTabController controller) {
+		super("Create Defect");
+		this.controller = controller;
+		putValue(MNEMONIC_KEY, KeyEvent.VK_C);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		controller.addCreateRequirementTab();
+	}
+
+}
