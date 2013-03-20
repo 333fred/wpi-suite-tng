15eb483013d457766f386ce128568af6066447f4
Fredric Silberberg <git@fsilberberg.com>
2013-03-20 19:27:55 -0400

    Changed the switch statements to use index instead of strings, so that it will compile under Java 1.6.

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/SaveRequirementAction.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/SaveRequirementAction.java
@@ -16,77 +16,83 @@ import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.models.Requirement;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view.note.SaveNoteController;
 
 public class SaveRequirementAction extends AbstractAction {
-	
+
 	private Requirement requirement;
 	private DetailView parentView;
-	
+
 	SaveRequirementAction(Requirement requirement, DetailView parentView) {
 		super("Add Requirement");
 		this.requirement = requirement;
 		this.parentView = parentView;
 	}
-	
+
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		AddRequirementController controller = new AddRequirementController();
-		
+
 		requirement.setName(parentView.textName.getText());
 		requirement.setDescription(parentView.textDescription.getText());
-		
-		switch((String)parentView.comboBoxPriority.getSelectedItem()) {
-		case "":
+
+		switch (parentView.comboBoxPriority.getSelectedIndex()) {
+		case 0:
 			requirement.setPriority(Priority.BLANK);
 			break;
-		case "High":
+		case 1:
 			requirement.setPriority(Priority.HIGH);
 			break;
-		case "Medium":
+		case 2:
 			requirement.setPriority(Priority.MEDIUM);
 			break;
-		case "Low":
+		case 3:
 			requirement.setPriority(Priority.LOW);
 			break;
+		default:
+			requirement.setPriority(Priority.BLANK);
 		}
 
-		switch((String)parentView.comboBoxType.getSelectedItem()) {
-		case "":
+		switch (parentView.comboBoxType.getSelectedIndex()) {
+		case 0:
 			requirement.setType(Type.BLANK);
 			break;
-		case "Epic":
+		case 1:
 			requirement.setType(Type.EPIC);
 			break;
-		case "Theme":
+		case 2:
 			requirement.setType(Type.THEME);
 			break;
-		case "User Story":
+		case 3:
 			requirement.setType(Type.USER_STORY);
 			break;
-		case "Non-functional":
+		case 4:
 			requirement.setType(Type.NON_FUNCTIONAL);
 			break;
-		case "Scenario":
+		case 5:
 			requirement.setType(Type.SCENARIO);
 			break;
+		default:
+			requirement.setType(Type.BLANK);
 		}
-				
-		switch((String)parentView.comboBoxStatus.getSelectedItem()) {
-		case "":
+
+		switch (parentView.comboBoxStatus.getSelectedIndex()) {
+		case 0:
 			requirement.setStatus(Status.BLANK);
 			break;
-		case "New":
+		case 1:
 			requirement.setStatus(Status.NEW);
 			break;
-		case "In Progress":
+		case 2:
 			requirement.setStatus(Status.IN_PROGRESS);
 			break;
-		case "Complete":
+		case 3:
 			requirement.setStatus(Status.COMPLETE);
 			break;
-		case "Deleted":
+		case 4:
 			requirement.setStatus(Status.DELETED);
 			break;
+		default:
+			requirement.setStatus(Status.BLANK);
 		}
-		
+
 		controller.AddRequirement(requirement);
 	}
 }
