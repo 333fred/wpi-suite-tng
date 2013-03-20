170bed22e248f9ccc76eabbf17df11d62b70569a
RiceX1055 <computers1055@gmail.com>
2013-03-20 15:47:08 -0400

    Added the toolbar to the main janeway module view

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/JanewayModule.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/JanewayModule.java
@@ -12,8 +12,10 @@ import javax.swing.JPanel;
 import edu.wpi.cs.wpisuitetng.janeway.modules.IJanewayModule;
 import edu.wpi.cs.wpisuitetng.janeway.modules.JanewayTabModel;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.requirement.view.RequirementListView;
-import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.tabs.MainTabView;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.tabs.MainTabController;
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.tabs.MainTabView;
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.toolbar.ToolbarController;
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.toolbar.ToolbarView;
 
 /**
  * @author Fredric
@@ -33,6 +35,12 @@ public class JanewayModule implements IJanewayModule {
 	/** The tab controller for tabView */
 	private MainTabController tabController;
 	
+	/** Toolbar view, displayed above the main tab view, and contains action buttons for the tabs */
+	private ToolbarView toolbarView;
+	
+	/** The controller for the toolbarView */
+	private ToolbarController toolbarController;
+	
 	/** Creates a new instance of JanewayModule, initializing the tabs to be displayed
 	 * 
 	 */
@@ -50,11 +58,17 @@ public class JanewayModule implements IJanewayModule {
 		//initialize TabController
 		tabController = new MainTabController(tabView);
 		
+		//initialize the toolbarView
+		toolbarView = new ToolbarView(tabController);
+		
+		//initialize the toolbar Controller
+		toolbarController = new ToolbarController(toolbarView, tabController);
+		
 		tabController.addTab("Requirements List", new ImageIcon(), requirementListView, "The list of requirements");
 		
 		
 		//create a new JanewayTabModel, passing in the tab view, and a new JPanel as the toolbar
-		JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), new JPanel(), tabView);
+		JanewayTabModel tab1 = new JanewayTabModel(getName(), new ImageIcon(), toolbarView, tabView);
 		
 		//add the tab to the list of tabs
 		tabs.add(tab1);
