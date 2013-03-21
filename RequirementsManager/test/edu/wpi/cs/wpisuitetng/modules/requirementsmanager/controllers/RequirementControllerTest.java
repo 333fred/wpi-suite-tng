8015577eab0b81082ea75712accc536f04b3f8ac
Fredric Silberberg <git@fsilberberg.com>
2013-03-20 20:21:47 -0400

    Added in a dummy test so that JUnit won't fail.

--- RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/controllers/RequirementControllerTest.java
+++ RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/controllers/RequirementControllerTest.java
@@ -7,5 +7,12 @@ import org.junit.Test;
 public class RequirementControllerTest {
 
 	//TODO: Test the requirement controllers
+	
+	// This is here so that JUnit won't say that this failed
+	// TODO: Replace with an actual test
+	@Test
+	public void notATest(){
+		assertEquals(1, 1);
+	}
 
 }
