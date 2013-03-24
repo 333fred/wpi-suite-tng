be5ec623b3ca0305069d0b0c450ddb9b73296839
cmkeane <cmkeane@wpi.edu>
2013-03-24 17:47:32 -0400

    Fixed input validator for editing a requirement

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/EditRequirementAction.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/EditRequirementAction.java
@@ -36,73 +36,99 @@ public class EditRequirementAction extends AbstractAction {
 	public void actionPerformed(ActionEvent e) {
 		SaveRequirementController controller = new SaveRequirementController(this.parentView);
 		
-		requirement.setName(parentView.textName.getText());
-		requirement.setDescription(parentView.textDescription.getText());
-
-		switch (parentView.comboBoxPriority.getSelectedIndex()) {
-		case 0:
-			requirement.setPriority(Priority.BLANK);
-			break;
-		case 1:
-			requirement.setPriority(Priority.HIGH);
-			break;
-		case 2:
-			requirement.setPriority(Priority.MEDIUM);
-			break;
-		case 3:
-			requirement.setPriority(Priority.LOW);
-			break;
-		default:
-			requirement.setPriority(Priority.BLANK);
+		if(!parentView.textName.getText().equals(""))
+		{
+			parentView.textName.setBackground(Color.WHITE);
+			parentView.textNameValid.setText("");
 		}
-
-		switch (parentView.comboBoxType.getSelectedIndex()) {
-		case 0:
-			requirement.setType(Type.BLANK);
-			break;
-		case 1:
-			requirement.setType(Type.EPIC);
-			break;
-		case 2:
-			requirement.setType(Type.THEME);
-			break;
-		case 3:
-			requirement.setType(Type.USER_STORY);
-			break;
-		case 4:
-			requirement.setType(Type.NON_FUNCTIONAL);
-			break;
-		case 5:
-			requirement.setType(Type.SCENARIO);
-			break;
-		default:
-			requirement.setType(Type.SCENARIO);
-		}
-
-		switch (parentView.comboBoxStatus.getSelectedIndex()) {
-		case 0:
-			requirement.setStatus(Status.NEW);
-			break;
-		case 1:
-			requirement.setStatus(Status.IN_PROGRESS);
-			break;
-		case 2:
-			requirement.setStatus(Status.OPEN);
-			break;			
-		case 3:
-			requirement.setStatus(Status.COMPLETE);
-			break;
-		case 4:
-			requirement.setStatus(Status.DELETED);
-			break;
-		case 5:
-			requirement.setStatus(Status.BLANK);
-			break;
-		}
-
-		controller.SaveRequirement(requirement);
-		this.parentView.getMainTabController().closeCurrentTab();
 		
+		if(!parentView.textDescription.getText().equals("")) 
+		{
+			parentView.textDescription.setBackground(Color.WHITE);
+			parentView.textDescriptionValid.setText("");
+		}
+		
+		if(!parentView.textName.getText().equals("") && !parentView.textDescription.getText().equals("")) 
+		{
+			requirement.setName(parentView.textName.getText());
+			requirement.setDescription(parentView.textDescription.getText());
+
+			switch (parentView.comboBoxPriority.getSelectedIndex()) {
+			case 0:
+				requirement.setPriority(Priority.BLANK);
+				break;
+			case 1:
+				requirement.setPriority(Priority.HIGH);
+				break;
+			case 2:
+				requirement.setPriority(Priority.MEDIUM);
+				break;
+			case 3:
+				requirement.setPriority(Priority.LOW);
+				break;
+			default:
+				requirement.setPriority(Priority.BLANK);
+			}
+
+			switch (parentView.comboBoxType.getSelectedIndex()) {
+			case 0:
+				requirement.setType(Type.BLANK);
+				break;
+			case 1:
+				requirement.setType(Type.EPIC);
+				break;
+			case 2:
+				requirement.setType(Type.THEME);
+				break;
+			case 3:
+				requirement.setType(Type.USER_STORY);
+				break;
+			case 4:
+				requirement.setType(Type.NON_FUNCTIONAL);
+				break;
+			case 5:
+				requirement.setType(Type.SCENARIO);
+				break;
+			default:
+				requirement.setType(Type.SCENARIO);
+			}
+
+			switch (parentView.comboBoxStatus.getSelectedIndex()) {
+			case 0:
+				requirement.setStatus(Status.NEW);
+				break;
+			case 1:
+				requirement.setStatus(Status.IN_PROGRESS);
+				break;
+			case 2:
+				requirement.setStatus(Status.OPEN);
+				break;			
+			case 3:
+				requirement.setStatus(Status.COMPLETE);
+				break;
+			case 4:
+				requirement.setStatus(Status.DELETED);
+				break;
+			case 5:
+				requirement.setStatus(Status.BLANK);
+				break;
+			}
+
+			controller.SaveRequirement(requirement);
+			this.parentView.getMainTabController().closeCurrentTab();
+		}
+		else {
+			if(parentView.textName.getText().equals(""))
+			{
+				parentView.textName.setBackground(new Color(255,100,100));
+				parentView.textNameValid.setText("Requirement must have name in order to save");
+			}
+			if(parentView.textDescription.getText().equals(""))
+			{
+				parentView.textDescription.setBackground(new Color(255,100,100));
+				parentView.textDescriptionValid.setText("Requirement must have description in order to save");
+			}
+		}		
 	}
 
 }
