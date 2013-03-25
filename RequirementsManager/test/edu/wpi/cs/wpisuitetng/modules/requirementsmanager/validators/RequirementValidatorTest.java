8e15706ceb2f65d0767e4f94c606ff4d9ed11ef5
Jason Whitehouse <jdb175@gmail.com>
2013-03-25 18:52:53 -0400

    Added iteration class, with dummy observer and controller for status implementation. Added more tests for vaidator.

--- RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/validators/RequirementValidatorTest.java
+++ RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/validators/RequirementValidatorTest.java
@@ -50,6 +50,7 @@ public class RequirementValidatorTest {
 	
 	@Before
 	public void init() throws Exception {
+		//initialize database and session info
 		testUser = new User(null, "testUser", null, -1);
 		testProject = new Project("test", "1");
 		mockSsid = "abc123";
@@ -57,7 +58,7 @@ public class RequirementValidatorTest {
 		db = new MockData(new HashSet<Object>());
 		db.save(testUser);
 
-		
+		//initialize good requirement and validator
 		validator = new RequirementValidator(db); 
 		goodRequirement = new Requirement();
 		goodRequirement.setName("Name");
@@ -65,49 +66,72 @@ public class RequirementValidatorTest {
 	}
 	
 	@Test
+	//Make sure that a null requirement will be rejected
 	public void testNullCreate() {
 		Requirement r = null;
 		checkNumIssues(1, r, defaultSession, RequirementActionMode.CREATE);
+		checkNumIssues(1, r, defaultSession, RequirementActionMode.EDIT);
 	}
 	
 	@Test
+	//Test that the most basic requirement (name and description only) will be valid
 	public void testGoodRequirementCreate() {
 		checkNoIssues(goodRequirement, defaultSession, RequirementActionMode.CREATE);
+		checkNoIssues(goodRequirement, defaultSession, RequirementActionMode.EDIT);
 	}
 	
 	@Test
+	//Ensure that a null name will cause rejection
 	public void testNullNameCreate() {
 		Requirement r  = new Requirement();
 		r.setName(null);
 		r.setDescription("A quality description");
 		checkNumIssues(1, r, defaultSession, RequirementActionMode.CREATE);
+		checkNumIssues(1, r, defaultSession, RequirementActionMode.EDIT);
 	}
 	
 	@Test
+	//Ensure that an empty name will cause rejection
+	public void testEmptyNameCreate() {
+		Requirement r  = new Requirement();
+		r.setName("");
+		r.setDescription("A quality description");
+		checkNumIssues(1, r, defaultSession, RequirementActionMode.CREATE);
+		checkNumIssues(1, r, defaultSession, RequirementActionMode.EDIT);
+	}
+	
+	@Test
+	//Ensure that a name longer than 100 characters is rejected
 	public void testLongNameCreate() {
 		Requirement r  = new Requirement();
 		r.setName(new String(new char[101]));
 		r.setDescription("A quality description");
 		checkNumIssues(1, r, defaultSession, RequirementActionMode.CREATE);
+		checkNumIssues(1, r, defaultSession, RequirementActionMode.EDIT);
 	}
 	
 	@Test
+	//Test that a null description will be rejected
 	public void testNoDescriptionCreate() {
 		Requirement r  = new Requirement();
 		r.setName("Name");
 		r.setDescription(null);
 		checkNumIssues(1, r, defaultSession, RequirementActionMode.CREATE);
+		checkNumIssues(1, r, defaultSession, RequirementActionMode.EDIT);
 	}
 	
 	@Test
-	public void testLinkedListCreate() {
+	//Test that an empty description will be rejected
+	public void testEmptyDescriptionCreate() {
 		Requirement r  = new Requirement();
 		r.setName("Name");
-		r.setDescription("Description");
-		checkNumIssues(0, r, defaultSession, RequirementActionMode.CREATE);
+		r.setDescription("");
+		checkNumIssues(1, r, defaultSession, RequirementActionMode.CREATE);
+		checkNumIssues(1, r, defaultSession, RequirementActionMode.EDIT);
 	}
-	
+		
 	@Test
+	//Test that a requirement created with no type will have its type set to BLANK
 	public void testTypeCreate() {
 		Requirement r  = new Requirement();
 		r.setName("Name");
@@ -116,7 +140,17 @@ public class RequirementValidatorTest {
 		
 		assertEquals(r.getType(),Type.BLANK);
 	}
-		
+	
+	@Test
+	//Test that a requirement with an invalid user will be rejected
+	public void testInvalidUser() {
+		Requirement r  = new Requirement();
+		r.setName("Name");
+		r.addTID("NotAUser");
+		r.setDescription("Description");
+		checkNumIssues(1, r, defaultSession, RequirementActionMode.CREATE);
+	}
+			
 	@Test
 	//Ensure that the status of any created requirement is 'NEW'
 	public void testStatusCreate() {
@@ -129,7 +163,19 @@ public class RequirementValidatorTest {
 	}
 	
 	@Test
-	//ensure that null linkedlists are properly initialized
+	//Ensure that the status of any edited requirement is not set to 'NEW' in validation
+	public void testStatusEdit() {
+		Requirement r  = new Requirement();
+		r.setName("Name");
+		r.setDescription("Description");
+		r.setStatus(Status.IN_PROGRESS);
+		checkNumIssues(0, r, defaultSession, RequirementActionMode.EDIT);
+		
+		assertEquals(r.getStatus(),Status.IN_PROGRESS);
+	}
+	
+	@Test
+	//ensure that null linkedlists are properly initialized on creation
 	public void testNullLinkedLists() {
 		Requirement r  = new Requirement();
 		r.setName("Name");
@@ -143,6 +189,31 @@ public class RequirementValidatorTest {
 		assertEquals(r.gettID(), new LinkedList<String>());
 	}
 	
+	@Test
+	//ensure that null linkedlists are properly initialized on edit
+	//This is a separate test because we are checking the modifications done by the validator
+	//instead of just the result
+	public void testNullLinkedListsEdit() {
+		Requirement r  = new Requirement();
+		r.setName("Name");
+		r.setDescription("Description");
+		checkNumIssues(0, r, defaultSession, RequirementActionMode.EDIT);
+		
+		assertEquals(r.getLog(), new LinkedList<String>());
+		assertEquals(r.getNotes(), new LinkedList<String>());
+		assertEquals(r.getpUID(), new LinkedList<Integer>());
+		assertEquals(r.getSubRequirements(), new LinkedList<Integer>());
+		assertEquals(r.gettID(), new LinkedList<String>());
+	}
+	
+	/**
+	 * 
+	 * @param num number of issues expected, if any other number are found will cause test failure
+	 * @param requirement the requirement to test
+	 * @param session the current session
+	 * @param mode the action mode of the validation (EDIT or CREATE)
+	 * @return a list of all validation issues found
+	 */
 	public List<ValidationIssue> checkNumIssues(int num, Requirement requirement, Session session, RequirementActionMode mode) {
 		List<ValidationIssue> issues;
 		try {
