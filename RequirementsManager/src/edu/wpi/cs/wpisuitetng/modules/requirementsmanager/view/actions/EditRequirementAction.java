76992ece2d9a1e31295d660994ef1631dbddfcbb
spkordell <mask@mask>
2013-03-28 18:44:03 -0400

    Attempting to add an iteration to a deleted requirement now returns an error

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/actions/EditRequirementAction.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/actions/EditRequirementAction.java
@@ -7,7 +7,6 @@ import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.commonenums.Priority;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.commonenums.Status;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.commonenums.Type;
 
-
 import javax.swing.AbstractAction;
 import javax.swing.JOptionPane;
 
@@ -16,101 +15,122 @@ import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.controllers.SaveRequir
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.models.Requirement;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view.DetailPanel;
 
-
-
 /**
- * The action for saving an edited requirement, used when "Save Requirement" button is pressed
+ * The action for saving an edited requirement, used when "Save Requirement"
+ * button is pressed
  * 
  * @author Chris
- *
+ * 
  */
 public class EditRequirementAction extends AbstractAction {
 
 	private Requirement requirement;
-    private DetailPanel parentView;
+	private DetailPanel parentView;
 
-    /**
-     * Constructor for EditRequirementAction
-     * Parent view is used to interact with the GUI
-     * Requirement is used to update the requirement being edited
-     *
-     * @param requirement
-     * @param parentView
-     */
-    public EditRequirementAction(Requirement requirement, DetailPanel parentView) {
+	/**
+	 * Constructor for EditRequirementAction Parent view is used to interact
+	 * with the GUI Requirement is used to update the requirement being edited
+	 * 
+	 * @param requirement
+	 * @param parentView
+	 */
+	public EditRequirementAction(Requirement requirement, DetailPanel parentView) {
 		super("Save Requirement");
 		this.requirement = requirement;
 		this.parentView = parentView;
 	}
 
-    /**
-     * This function is called when the save requirement button is pressed on a requirement being edited
-     * It validates input in sets responses to the user if an error is made
-     *
-     * @param e 
-     */
+	/**
+	 * This function is called when the save requirement button is pressed on a
+	 * requirement being edited It validates input in sets responses to the user
+	 * if an error is made
+	 * 
+	 * @param e
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		SaveRequirementController controller = new SaveRequirementController(this.parentView);
-		
-		//Checks to make sure the name entered is valid and updates the GUI if it is
-		if(!parentView.getTextName().getText().trim().equals(""))
-		{
+		SaveRequirementController controller = new SaveRequirementController(
+				this.parentView);
+
+		// Checks to make sure the name entered is valid and updates the GUI if
+		// it is
+		if (!parentView.getTextName().getText().trim().equals("")) {
 			parentView.getTextName().setBackground(Color.WHITE);
 			parentView.getTextNameValid().setText("");
 		}
-		
-		//Checks to make sure the description entered is valid and updates the GUI if it is
-		if(!parentView.getTextDescription().getText().trim().equals("")) 
-		{
+
+		// Checks to make sure the description entered is valid and updates the
+		// GUI if it is
+		if (!parentView.getTextDescription().getText().trim().equals("")) {
 			parentView.getTextDescription().setBackground(Color.WHITE);
 			parentView.getTextDescriptionValid().setText("");
 		}
-		
-		//Checks to make sure that both the name and descriptions are not empty, and attempts to save the requirements
-		if(!parentView.getTextName().getText().trim().equals("") && !parentView.getTextDescription().getText().trim().equals("")) 
-		{
-			requirement.setName(parentView.getTextName().getText().trim());
-			requirement.setDescription(parentView.getTextDescription().getText());
-			requirement.setUsers(parentView.getAssignedUsers());
-			
-			try {
-				requirement.setIteration(Integer.parseInt(parentView.getTextIteration().getText()));
-				
+
+		if (parentView.getComboBoxStatus().getSelectedItem().equals("Deleted")
+				&& Integer.parseInt(parentView.getTextIteration().getText()) != -1) {
+			parentView
+					.displaySaveError("Cannot add iteration to deleted project. Set box value to \"-1\".");
+		} else {
+
+			// Checks to make sure that both the name and descriptions are not
+			// empty, and attempts to save the requirements
+			if (!parentView.getTextName().getText().trim().equals("")
+					&& !parentView.getTextDescription().getText().trim()
+							.equals("")) {
+				requirement.setName(parentView.getTextName().getText().trim());
+				requirement.setDescription(parentView.getTextDescription()
+						.getText());
+				requirement.setUsers(parentView.getAssignedUsers());
+
 				try {
-					requirement.setPriority(Priority.valueOf(parentView.getComboBoxPriority().getSelectedItem().toString().toUpperCase().replaceAll(" ", "_")));
-				} catch(IllegalArgumentException except) {
-					requirement.setPriority(Priority.BLANK);
+					requirement.setIteration(Integer.parseInt(parentView
+							.getTextIteration().getText()));
+
+					try {
+						requirement
+								.setPriority(Priority.valueOf(parentView
+										.getComboBoxPriority()
+										.getSelectedItem().toString()
+										.toUpperCase().replaceAll(" ", "_")));
+					} catch (IllegalArgumentException except) {
+						requirement.setPriority(Priority.BLANK);
+					}
+
+					try {
+						requirement.setType(Type.valueOf(parentView
+								.getComboBoxType().getSelectedItem().toString()
+								.toUpperCase().replaceAll(" ", "_")));
+					} catch (IllegalArgumentException except) {
+						requirement.setType(Type.BLANK);
+					}
+
+					requirement.setStatus(Status.valueOf(parentView
+							.getComboBoxStatus().getSelectedItem().toString()
+							.toUpperCase().replaceAll(" ", "_")));
+
+					controller.SaveRequirement(requirement, true);
+					// Done by the observer now
+					// //this.parentView.getMainTabController().closeCurrentTab();
+				} catch (NumberFormatException excep) {
+					parentView
+							.displaySaveError("Iteration must be an integer value");
 				}
-				
-				try {
-					requirement.setType(Type.valueOf(parentView.getComboBoxType().getSelectedItem().toString().toUpperCase().replaceAll(" ", "_")));
-				} catch(IllegalArgumentException except) {
-					requirement.setType(Type.BLANK);
+			} else {
+				if (parentView.getTextName().getText().trim().equals("")) {
+					parentView.getTextName().setBackground(
+							new Color(243, 243, 209));
+					parentView.getTextNameValid().setText(
+							"** Field must be non-blank **");
 				}
-				
-				requirement.setStatus(Status.valueOf(parentView.getComboBoxStatus().getSelectedItem().toString().toUpperCase().replaceAll(" ", "_")));
-			
-				controller.SaveRequirement(requirement,true);
-			//Done by the observer now //this.parentView.getMainTabController().closeCurrentTab();
-			} catch (NumberFormatException excep) {
-				parentView.displaySaveError("Iteration must be an integer value");
+				if (parentView.getTextDescription().getText().trim().equals("")) {
+					parentView.getTextDescription().setBackground(
+							new Color(243, 243, 209));
+					parentView.getTextDescriptionValid().setText(
+							"** Field must be non-blank **");
+
+				}
 			}
 		}
-		else {
-			if(parentView.getTextName().getText().trim().equals(""))
-			{
-				parentView.getTextName().setBackground(new Color(243, 243, 209));
-				parentView.getTextNameValid().setText("** Field must be non-blank **");
-			}
-			if(parentView.getTextDescription().getText().trim().equals(""))
-			{
-				parentView.getTextDescription().setBackground(new Color(243, 243, 209));
-				parentView.getTextDescriptionValid().setText("** Field must be non-blank **");
-
-			}
-		}		
 	}
 
 }
-
