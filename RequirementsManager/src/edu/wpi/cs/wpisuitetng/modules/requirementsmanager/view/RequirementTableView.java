21a999b9e833046b16a49cf47235dad6c029a35a
ahwoodayrd <ahwoodyard@wpi.edu>
2013-03-31 15:37:38 -0400

    Added check that requirement is not already displayed.  This version uses the ID and not the name.

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/RequirementTableView.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/RequirementTableView.java
@@ -8,6 +8,7 @@ import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Vector;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -26,43 +27,50 @@ import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view.actions.OpenRequi
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view.actions.RefreshAction;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view.actions.ViewRequirementAction;
 
-/** RequirementListView is the basic GUI that will display a list of the current Requirements, fetched from the database.
- * It will allow the user to select a given requirement and view/edit it.
- * It also allows users to create new requirements, and refresh the list of requirements
+/**
+ * RequirementListView is the basic GUI that will display a list of the current
+ * Requirements, fetched from the database. It will allow the user to select a
+ * given requirement and view/edit it. It also allows users to create new
+ * requirements, and refresh the list of requirements
  * 
  * @author Steve, Mitchell
- *
+ * 
  */
 @SuppressWarnings("serial")
-public class RequirementTableView extends FocusableTab implements IToolbarGroupProvider, IReceivedAllRequirementNotifier {	
+public class RequirementTableView extends FocusableTab implements
+		IToolbarGroupProvider, IReceivedAllRequirementNotifier {
 
 	/** The MainTabController that this view is inside of */
 	private final MainTabController tabController;
-	
+
 	/** Controller for receiving all requirements from the server */
 	private RetrieveAllRequirementsController retreiveAllRequirementsController;
-	
+
 	/** The list of requirements that the view is displaying */
-	private Requirement[] requirements;	
-	
-	/** The View that will display on the toolbar and contain buttons relating to this view */
+	private Requirement[] requirements;
+
+	/**
+	 * The View that will display on the toolbar and contain buttons relating to
+	 * this view
+	 */
 	private ToolbarGroupView toolbarView;
-	
+
 	/** The View and Refresh buttons used on the toolbar */
 	private JButton butView;
 	private JButton butRefresh;
-	
+
 	/** Flag used to make paint only refresh the requirements once, on load */
 	private boolean firstPaint;
-	
+
 	@SuppressWarnings("rawtypes")
 	private Vector<Vector> rowData;
-	
+
 	private JTable table;
-	
+
 	private IterationTreeView iterationTree;
-	
-	/** Constructor for a RequirementTableView
+
+	/**
+	 * Constructor for a RequirementTableView
 	 * 
 	 * 
 	 */
@@ -70,73 +78,76 @@ public class RequirementTableView extends FocusableTab implements IToolbarGroupP
 	public RequirementTableView(MainTabController tabController) {
 		this.tabController = tabController;
 		this.iterationTree = new IterationTreeView();
-		
+
 		firstPaint = false;
-		//create the Retreive All Requiments Controller
-		retreiveAllRequirementsController = new RetrieveAllRequirementsController(this);
-		//init the toolbar group
+		// create the Retreive All Requiments Controller
+		retreiveAllRequirementsController = new RetrieveAllRequirementsController(
+				this);
+		// init the toolbar group
 		initializeToolbarGroup();
-		
+
 		requirements = new Requirement[0];
-	
+
 		JPanel mainPanel = new JPanel(new BorderLayout());
-		//setLayout(new BorderLayout(0, 0));
+		// setLayout(new BorderLayout(0, 0));
 		GridLayout mainLayout = new GridLayout(0, 1);
-		setLayout(mainLayout);	
-		
-	    Vector<String> columnNames = new Vector<String>();
-	    columnNames.addElement("Name");
-	    columnNames.addElement("Type");
-	    columnNames.addElement("Priority");
-	    columnNames.addElement("Status");
-	    columnNames.addElement("Iteration");
-	    columnNames.addElement("Effort");
-	    columnNames.addElement("Estimate");
-	    columnNames.addElement("Release Number");
-		
-	    this.rowData = new Vector<Vector>();
-	    
+		setLayout(mainLayout);
+
+		Vector<String> columnNames = new Vector<String>();
+		columnNames.addElement("Name");
+		columnNames.addElement("Type");
+		columnNames.addElement("Priority");
+		columnNames.addElement("Status");
+		columnNames.addElement("Iteration");
+		columnNames.addElement("Effort");
+		columnNames.addElement("Estimate");
+		columnNames.addElement("Release Number");
+
+		this.rowData = new Vector<Vector>();
+
 		this.table = new JTable(rowData, columnNames) {
 			private static final long serialVersionUID = 1L;
-	        public boolean isCellEditable(int row, int column) {                
-	        	return false;               
-		    };
+
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			};
 		};
-				
+
 		JScrollPane scrollPane = new JScrollPane(this.table);
 		this.table.setFillsViewportHeight(true);
-		mainPanel.add(scrollPane,BorderLayout.CENTER);
-		
-		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,iterationTree,mainPanel);
+		mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+		JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,
+				iterationTree, mainPanel);
 		add(splitPane);
 
 		splitPane.setResizeWeight(0.1);
-		
-		//Add double click event listener		
-		  this.table.addMouseListener(new MouseAdapter() {
-			   public void mouseClicked(MouseEvent e) {
-			      if (e.getClickCount() == 2) {
-			         JTable target = (JTable)e.getSource();
-			         int row = target.getSelectedRow();
-			         onDoubleClick(row);
-			         }
-			   }
-			});
-		  
+
+		// Add double click event listener
+		this.table.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 2) {
+					JTable target = (JTable) e.getSource();
+					int row = target.getSelectedRow();
+					onDoubleClick(row);
+				}
+			}
+		});
+
 	}
-	
-	
+
 	/*
 	 * @author Steve Kordell
-	 * 
 	 */
-	//TODO: More documentation
-	void onDoubleClick(int index) {	
-		//update to use this function instead
+	// TODO: More documentation
+	void onDoubleClick(int index) {
+		// update to use this function instead
 		viewRequirement(index);
 	}
-	
-	/** Initializes the toolbar group, and adds the buttons that will be displayed to it.
+
+	/**
+	 * Initializes the toolbar group, and adds the buttons that will be
+	 * displayed to it.
 	 * 
 	 * 
 	 */
@@ -144,26 +155,31 @@ public class RequirementTableView extends FocusableTab implements IToolbarGroupP
 		toolbarView = new ToolbarGroupView("Requirements");
 		butView = new JButton("Edit Requirement");
 		butRefresh = new JButton("Refresh");
-		
+
 		butRefresh.setAction(new RefreshAction(this));
 		butView.setAction(new ViewRequirementAction(this));
-		//create and add the buttons that will be displayed
+		// create and add the buttons that will be displayed
 		toolbarView.getContent().add(butView);
 		toolbarView.getContent().add(butRefresh);
-		//set the width of the group so it is not too long
-		toolbarView.setPreferredWidth((int)(butView.getPreferredSize().getWidth() + butRefresh.getPreferredSize().getWidth() + 40));
+		// set the width of the group so it is not too long
+		toolbarView.setPreferredWidth((int) (butView.getPreferredSize()
+				.getWidth() + butRefresh.getPreferredSize().getWidth() + 40));
 	}
-	
-	/** Takes a list of requirements, and turns them into a list of Strings that the View will display
+
+	/**
+	 * Takes a list of requirements, and turns them into a list of Strings that
+	 * the View will display
 	 * 
-	 * @param requirements The list of requirements 
-	 * @return a list of requirements in string form, that can be shown on the JList
+	 * @param requirements
+	 *            The list of requirements
+	 * @return a list of requirements in string form, that can be shown on the
+	 *         JList
 	 */
-	
-	private void parseRequirements() {		
-	
+
+	private void parseRequirements() {
+
 		this.rowData.clear();
-		for(int i = 0; i < requirements.length; i++){
+		for (int i = 0; i < requirements.length; i++) {
 			Vector<String> row = new Vector<String>();
 			row.addElement(requirements[i].getName());
 			row.addElement(requirements[i].getType().toString());
@@ -176,141 +192,164 @@ public class RequirementTableView extends FocusableTab implements IToolbarGroupP
 			this.rowData.add(row);
 		}
 	}
-	
-	/** Function to retreive the requirements from the server
+
+	/**
+	 * Function to retreive the requirements from the server
 	 * 
-	 * Mostly a place holder function until the backend is implemented
-	 * The list view will automaticaly be updated once the request comes back
+	 * Mostly a place holder function until the backend is implemented The list
+	 * view will automaticaly be updated once the request comes back
 	 * 
 	 */
-	
+
 	private void getRequirementsFromServer() {
 		System.out.println("We are getting requirements from the server");
 		retreiveAllRequirementsController.getAll();
 	}
-	
-	/** Updates the list view acording to the values in the Requirements Array
+
+	/**
+	 * Updates the list view acording to the values in the Requirements Array
 	 * 
 	 * */
-	
+
 	private void updateListView() {
 		System.out.println("We are updating the table view");
-		//update the string array
+		// update the string array
 		parseRequirements();
-		//set the list values of the requirementsList to the values in the String aray
-		//requirementsList.setListData(listValues.toArray(new String[0]));
-		//invalidate the list so it is forced to be redrawn
-		//this.table.invalidate();
+		// set the list values of the requirementsList to the values in the
+		// String aray
+		// requirementsList.setListData(listValues.toArray(new String[0]));
+		// invalidate the list so it is forced to be redrawn
+		// this.table.invalidate();
 		this.table.repaint();
 	}
 
-	/** Inherited from IToolBarGroup Provider, Provides the toolbar buttons used by this view
+	/**
+	 * Inherited from IToolBarGroup Provider, Provides the toolbar buttons used
+	 * by this view
 	 * 
 	 */
-	
+
 	@Override
 	public ToolbarGroupView getGroup() {
 		return toolbarView;
 	}
-	
-	/** Retreives the list of requirements from the server, then updates the List View acordingly 
+
+	/**
+	 * Retreives the list of requirements from the server, then updates the List
+	 * View acordingly
 	 * 
 	 */
-	
+
 	public void refresh() {
-		//retreive a new copy of requirements, and update the list view
+		// retreive a new copy of requirements, and update the list view
 		getRequirementsFromServer();
 	}
-	
-	/** Open a new tab containing a view of the selected requirement in the list view
-	   TODO: Possibly implement view requirement mode
+
+	/**
+	 * Open a new tab containing a view of the selected requirement in the list
+	 * view TODO: Possibly implement view requirement mode
 	 */
-	
+
 	public void viewRequirement() {
-		//obtain the currently selected requirement
+		// obtain the currently selected requirement
 		int selectedIndex = this.table.getSelectedRow();
-										
+
 		if (selectedIndex < 0) {
-			//nothing is currently selected
+			// nothing is currently selected
 			return;
 		}
-		
+
 		viewRequirement(selectedIndex);
 	}
-	
-	/** Views a requirement with the given index
+
+	/**
+	 * Views a requirement with the given index
 	 * 
-	 * @param index Index of the requirement to view
+	 * @param index
+	 *            Index of the requirement to view
 	 */
-	
+
 	public void viewRequirement(int index) {
 		boolean requirementIsOpen = false;
-		
-				
-		if (index <0 || index >= requirements.length) {
-			//invalid index
+
+		if (index < 0 || index >= requirements.length) {
+			// invalid index
 			System.out.println("Invalid index");
 		}
-		
-		//get the requirement to update from the array
+		System.out.println("Trying to view a requirement");
+		// get the requirement to update from the array
 		Requirement requirementToFetch = requirements[index];
-		
-		for (int i = 0; i < this.tabController.getTabView().getTabCount(); i++) {
-			if (this.tabController.getTabView().getTitleAt(i).equals(requirementToFetch.getName())) {
+
+		// Check to make sure the requirement is not already being
+		// displayed. This is assuming that the list view is displayed in
+		// the left most tab, index 0
+		for (int i = 1; i < this.tabController.getTabView().getTabCount(); i++) {
+			if (((((DetailPanel) this.tabController.getTabView()
+					.getComponentAt(i))).getModel().getrUID()) == (requirementToFetch
+					.getrUID())) {
 				this.tabController.switchToTab(i);
 				requirementIsOpen = true;
 			}
 		}
-		
+
 		if (!requirementIsOpen) {
 			// create the controller for fetching the new requirement
 			RetrieveRequirementByIDController retreiveRequirementController = new RetrieveRequirementByIDController(
-					new OpenRequirementTabAction(tabController, requirementToFetch));
-			
-			//get the requirement from the server
+					new OpenRequirementTabAction(tabController,
+							requirementToFetch));
+
+			// get the requirement from the server
 			retreiveRequirementController.get(requirementToFetch.getrUID());
 		}
 	}
 
-	/** The updated requirements data has been received, update the list
+	/**
+	 * The updated requirements data has been received, update the list
 	 * 
-	 * @param requirements The new requirement data
+	 * @param requirements
+	 *            The new requirement data
 	 */
-	
+
 	@Override
 	public void receivedData(Requirement[] requirements) {
 		this.requirements = requirements;
 		updateListView();
 	}
 
-	/** There was an error processing the data request.
-	 * TODO: Implement what we do
-	 *
+	/**
+	 * There was an error processing the data request. TODO: Implement what we
+	 * do
+	 * 
 	 */
-	
+
 	@Override
 	public void errorReceivingData(String RetrieveAllRequirementsRequestObserver) {
-		System.out.println("Received an error fecting from server: |" + RetrieveAllRequirementsRequestObserver);
+		System.out.println("Received an error fecting from server: |"
+				+ RetrieveAllRequirementsRequestObserver);
 	}
 
-	/** Method that updates the content of the list view when this tab gains focus
+	/**
+	 * Method that updates the content of the list view when this tab gains
+	 * focus
 	 * 
 	 */
-	
+
 	public void onTabFocus() {
-		refresh();		
+		refresh();
 	}
-	
-	/** Retrieve the requirements from the server when the GUI is first painted
+
+	/**
+	 * Retrieve the requirements from the server when the GUI is first painted
 	 * 
-	 * @param g The Graphics object
+	 * @param g
+	 *            The Graphics object
 	 * 
 	 */
-	
+
 	public void paint(Graphics g) {
-		//call super so there is no change to functionality
+		// call super so there is no change to functionality
 		super.paint(g);
-		//refresh the requirements, the first time this is called
+		// refresh the requirements, the first time this is called
 		if (!firstPaint) {
 			refresh();
 			firstPaint = true;
