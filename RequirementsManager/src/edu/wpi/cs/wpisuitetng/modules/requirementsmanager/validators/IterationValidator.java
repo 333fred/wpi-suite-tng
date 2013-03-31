910b70b63bb9dbbfd86212d9b147b29e433cdc00
Fredric Silberberg <github@fsilberberg.com>
2013-03-31 13:13:55 -0400

    Implemented Entity manager for iterations

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/validators/IterationValidator.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/validators/IterationValidator.java
@@ -1,5 +1,16 @@
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.validators;
 
+import java.util.List;
+
+import edu.wpi.cs.wpisuitetng.Session;
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.commonenums.IterationActionMode;
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.models.Iteration;
+
 public class IterationValidator {
+
+	public List<ValidationIssue> validate(Session s, Iteration i,
+			IterationActionMode mode) {
+		return null;
+	}
 
 }
