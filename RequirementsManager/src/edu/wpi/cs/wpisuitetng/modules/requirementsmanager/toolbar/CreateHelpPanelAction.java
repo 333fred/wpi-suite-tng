3bd66d291db21b03166d988165ab9538f1902504
Nicholas Massa <nbmassa@wpi.edu>
2013-04-01 18:50:20 -0400

    User Manual no longer opens a new tab

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/toolbar/CreateHelpPanelAction.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/toolbar/CreateHelpPanelAction.java
@@ -8,8 +8,12 @@
 
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.toolbar;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.swing.AbstractAction;
 
@@ -35,7 +39,29 @@ public class CreateHelpPanelAction extends AbstractAction {
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		controller.addHelpPanelTab();
+		//controller.addHelpPanelTab();     Commented out, in favor of not opening a tab!!!
+		URL index = null; //Will be our main index page
+		
+		File input = new File("index.html"); //Html file is our input
+		String path = new String("file:///"+input.getAbsolutePath()); //Grab the url of the path
+		System.out.println(path); //Make sure the path is correct!
+		
+		try {
+			index = new URL(path); //Try to set our index url given the path
+		} catch (MalformedURLException e1) {
+			System.out.println("Error setting URL path!");
+			e1.printStackTrace();
+		}
+		//Create a desktop type in order to launch the user's default browser
+		 Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+		    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) { //If desktop was created and a browser is supported
+		        try {
+		            desktop.browse(index.toURI()); //Convert link to identifier and launch default browser
+		        } catch (Exception f) {
+		        	System.out.println("Error launching browser!");
+		            f.printStackTrace();
+		        }
+		    }
 	}
 
 }
