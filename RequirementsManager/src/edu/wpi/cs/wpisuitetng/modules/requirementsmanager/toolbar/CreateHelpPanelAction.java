55e255db0a55278b32cddd9aa26a8dcae2e05dd6
Nicholas Massa <nbmassa@wpi.edu>
2013-04-03 18:46:10 -0400

    Made the browser opening sequence simpler / not path dependent with Alex W

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/toolbar/CreateHelpPanelAction.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/toolbar/CreateHelpPanelAction.java
@@ -34,28 +34,18 @@ public class CreateHelpPanelAction extends AbstractAction {
 	public CreateHelpPanelAction(MainTabController controller) {
 		super("User Manual");
 		this.controller = controller;
-		putValue(MNEMONIC_KEY, KeyEvent.VK_U);
+		putValue(MNEMONIC_KEY, KeyEvent.VK_F1);
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		//controller.addHelpPanelTab();     Commented out, in favor of not opening a tab!!!
-		URL index = null; //Will be our main index page
-		
+	public void actionPerformed(ActionEvent e) {		
 		File input = new File("index.html"); //Html file is our input
-		String path = new String("file:///"+input.getAbsolutePath()); //Grab the url of the path
 		
-		try {
-			index = new URL(path); //Try to set our index url given the path
-		} catch (MalformedURLException e1) {
-			System.out.println("Error setting URL path!");
-			e1.printStackTrace();
-		}
 		//Create a desktop type in order to launch the user's default browser
 		 Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
 		    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) { //If desktop was created and a browser is supported
 		        try {
-		            desktop.browse(index.toURI()); //Convert link to identifier and launch default browser
+		            desktop.open(input); //Convert link to identifier and launch default browser
 		        } catch (Exception f) {
 		        	System.out.println("Error launching browser!");
 		            f.printStackTrace();
