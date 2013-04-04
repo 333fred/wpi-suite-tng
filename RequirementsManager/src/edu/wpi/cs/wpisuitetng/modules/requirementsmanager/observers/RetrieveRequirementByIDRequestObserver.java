011bd0fb637ea7431b51201e2a0d9846c9889ee0
ahwoodayrd <ahwoodyard@wpi.edu>
2013-04-04 13:31:37 -0400

    Added license and contributor information to all files in observers package

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/observers/RetrieveRequirementByIDRequestObserver.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/observers/RetrieveRequirementByIDRequestObserver.java
@@ -1,6 +1,14 @@
-/**
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite: Team Swagasarus
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Jason Whitehouse
+ *******************************************************************************/
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.observers;
 
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.controllers.RetrieveRequirementByIDController;
