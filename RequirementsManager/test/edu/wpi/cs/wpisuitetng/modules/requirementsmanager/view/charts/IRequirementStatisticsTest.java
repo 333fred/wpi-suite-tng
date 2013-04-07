600962e05067a5ddaf13868ad540d69ee22a9c58
ccorwyn <alex@gorowara.(none)>
2013-04-06 23:00:26 -0400

    added file for IRequirementStatistics test

---
+++ RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/charts/IRequirementStatisticsTest.java
@@ -0,0 +1,20 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite: Team Swagasarus
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Alex Gorowara
+ *******************************************************************************/
+package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view.charts;
+
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view.charts.IRequirementStatistics;
+
+public class IRequirementStatisticsTest {
+
+	// TODO: work out how to force the RequirementDatabase class to use a mock project
+	
+}
