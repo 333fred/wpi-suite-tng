10b92186331b0f4dfbf37761b2f350ae27e5b184
ahwoodayrd <ahwoodyard@wpi.edu>
2013-04-08 20:41:40 -0400

    Fixed formatting and license typos.

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/entitymanagers/UserEntityManager.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/entitymanagers/UserEntityManager.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 -- WPI Suite: Team Swagasarus
+ * Copyright (c) 2013 -- WPI Suite: Team Swagasaurus
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Fredric
+ *    @author Fredric
  *******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.entitymanagers;
@@ -28,8 +28,6 @@ import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.models.StringListModel
 
 /**
  * Simple entity manager to allow for getting all user names from the database
- * 
- * @author Fredric
  */
 
 public class UserEntityManager implements EntityManager<StringListModel> {
--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/exceptions/RequirementNotFoundException.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/exceptions/RequirementNotFoundException.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 -- WPI Suite: Team Swagasarus
+ * Copyright (c) 2013 -- WPI Suite: Team Swagasaurus
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -7,13 +7,12 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Fredric
+ *    @author Fredric
  *******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.exceptions;
 
 /**
- * @author Fredric
  *
  */
 public class RequirementNotFoundException extends Exception {
