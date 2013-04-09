445ee83d3fe756899d977076a6fd2154d6eb1089
spkordell <mask@mask>
2013-04-09 19:32:29 -0400

    Began Implementing Chart Selection Panel

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/charts/StatView.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/charts/StatView.java
@@ -4,27 +4,93 @@ package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view.charts;
 
 import java.awt.BorderLayout;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
 
+import javax.swing.ButtonGroup;
+import javax.swing.JComboBox;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JSplitPane;
+import javax.swing.SpringLayout;
 
-import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.chart.demo.PieChartDemo1;
 import org.jfree.chart.plot.PiePlot;
-import org.jfree.data.general.DefaultPieDataset;
-import org.jfree.data.general.PieDataset;
-import org.jfree.ui.RefineryUtilities;
+
 
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.tabs.Tab;
 
-public class StatView extends Tab {
+@SuppressWarnings("serial")
+public class StatView extends Tab implements ActionListener {
 
-public StatView() {
+	private JComboBox comboBoxStatisticType;
+	
+	public StatView() {
 		this.setLayout(new BorderLayout());
-		JPanel chart = new ChartPanel(createChart());
-        add(chart,BorderLayout.CENTER);
-}
+		JPanel chart = new ChartPanel(createPieStatusChart());
+		
+		JSplitPane mainPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT,chart,buildSidePanel());
+		mainPane.setResizeWeight(0.75);
+		add(mainPane);
+	}
+
+
+	public JPanel buildSidePanel() {
+		final int VERTICAL_PADDING = 5;
+		final int HORIZONTAL_PADDING = 5;
+		final int FAR = 5;
+		
+		SpringLayout sidePanelLayout = new SpringLayout();
+		JPanel sidePanel = new JPanel(sidePanelLayout);		
+		
+		JLabel lblStatisticType = new JLabel("Statistic Type");
+		JLabel lblChartType = new JLabel("Chart Type");
+		
+		String[] availableStatisticTypes = {"Assignees","Iterations","Status"};
+		comboBoxStatisticType = new JComboBox(availableStatisticTypes);
+	    comboBoxStatisticType.addActionListener(this);
+		
+		JRadioButton makePieRadio = new JRadioButton("Pie Chart");
+	    makePieRadio.setMnemonic(KeyEvent.VK_P);
+	    makePieRadio.setActionCommand("Pie Chart");
+	    makePieRadio.addActionListener(this);
+	    makePieRadio.setSelected(true);
+	    
+		JRadioButton makeBarRadio = new JRadioButton("Bar Chart");
+		makeBarRadio.setMnemonic(KeyEvent.VK_B);
+		makeBarRadio.setActionCommand("Bar Chart");
+		makeBarRadio.addActionListener(this);
+	
+		ButtonGroup group = new ButtonGroup();
+		group.add(makePieRadio);
+		group.add(makeBarRadio);
+		
+		sidePanel.add(lblStatisticType);
+		sidePanel.add(lblChartType);
+		sidePanel.add(comboBoxStatisticType);
+		sidePanel.add(makePieRadio);
+		sidePanel.add(makeBarRadio);
+		
+		sidePanelLayout.putConstraint(SpringLayout.NORTH,lblStatisticType,VERTICAL_PADDING,SpringLayout.NORTH,sidePanel);
+		sidePanelLayout.putConstraint(SpringLayout.WEST,lblStatisticType,HORIZONTAL_PADDING,SpringLayout.WEST,sidePanel);
+		
+		sidePanelLayout.putConstraint(SpringLayout.NORTH,comboBoxStatisticType,VERTICAL_PADDING,SpringLayout.SOUTH,lblStatisticType);
+		sidePanelLayout.putConstraint(SpringLayout.WEST,comboBoxStatisticType,HORIZONTAL_PADDING,SpringLayout.WEST,sidePanel);
+		
+		sidePanelLayout.putConstraint(SpringLayout.NORTH,lblChartType,VERTICAL_PADDING+FAR,SpringLayout.SOUTH,comboBoxStatisticType);
+		sidePanelLayout.putConstraint(SpringLayout.WEST,lblChartType,HORIZONTAL_PADDING,SpringLayout.WEST,sidePanel);	
+		
+		sidePanelLayout.putConstraint(SpringLayout.NORTH,makePieRadio,VERTICAL_PADDING+FAR,SpringLayout.SOUTH,lblChartType);
+		sidePanelLayout.putConstraint(SpringLayout.WEST,makePieRadio,HORIZONTAL_PADDING,SpringLayout.WEST,sidePanel);
+		
+		sidePanelLayout.putConstraint(SpringLayout.NORTH,makeBarRadio,VERTICAL_PADDING,SpringLayout.SOUTH,makePieRadio);
+		sidePanelLayout.putConstraint(SpringLayout.WEST,makeBarRadio,HORIZONTAL_PADDING,SpringLayout.WEST,sidePanel);
+		
+		return sidePanel;
+	}
     
     /**
 * Creates a chart.
@@ -33,24 +99,20 @@ public StatView() {
 *
 * @return A chart.
 */
-    private static JFreeChart createChart() {
-        /*
-        JFreeChart chart = ChartFactory.createPieChart(
-            "Pie Chart Demo 1", // chart title
-            dataset, // data
-            true, // include legend
-            true,
-            false
-        );
-
+    private static JFreeChart createPieStatusChart() {    	
+    	StatusRequirementStatistics statusRequirementStatistics = new StatusRequirementStatistics();
+		JFreeChart chart = statusRequirementStatistics.buildPieChart();
         PiePlot plot = (PiePlot) chart.getPlot();
         plot.setLabelFont(new Font("SansSerif", Font.PLAIN, 12));
         plot.setNoDataMessage("No data available");
-        plot.setCircular(false);
+        plot.setCircular(true);
         plot.setLabelGap(0.02);
         return chart;
-        */
-    	StatusRequirementStatistics statusRequirementStatistics = new StatusRequirementStatistics();
-    	return statusRequirementStatistics.buildPieChart();
     }
+    
+    public void actionPerformed(ActionEvent e) {
+    	System.out.println(e.getActionCommand()); 
+    }
+
+
 }
