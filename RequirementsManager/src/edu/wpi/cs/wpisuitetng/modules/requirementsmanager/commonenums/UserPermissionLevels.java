300a8e4c039c12914b387fb41f5a3aeb29aa6721
ahwoodayrd <ahwoodyard@wpi.edu>
2013-04-14 16:15:19 -0400

    Added enum for permissions

---
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/commonenums/UserPermissionLevels.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite: Team Swagasaurus
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    @author Alex
+ *******************************************************************************/
+
+package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.commonenums;
+
+public class UserPermissionLevels {
+
+}
