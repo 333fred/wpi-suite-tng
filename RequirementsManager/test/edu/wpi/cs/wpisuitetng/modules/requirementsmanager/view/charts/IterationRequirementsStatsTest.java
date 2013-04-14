cc33e4382a8fbd961777095d0bd3d4d38cfdf99c
ahwoodayrd <ahwoodyard@wpi.edu>
2013-04-14 16:58:50 -0400

    Added statistics tests.

--- RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/charts/IterationRequirementsStatsTest.java
+++ RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/charts/IterationRequirementsStatsTest.java
@@ -7,32 +7,23 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    @author Alex
+ *    @author Alex Woodyard
  *******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view.charts;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.JFreeChart;
-import org.jfree.chart.axis.NumberAxis;
-import org.jfree.chart.plot.CategoryPlot;
-import org.jfree.chart.plot.PlotOrientation;
-import org.junit.Before;
 import org.junit.Test;
 
 public class IterationRequirementsStatsTest {
 
 	IterationRequirementStatistics data = new IterationRequirementStatistics();
-	
+
 	@Test
-	public void iterationLineChart(){
-		JFreeChart chart = ChartFactory.createLineChart("Requirements by Iteration", "Iteration", "Requirements", data.toCategoryDataset("Iteration"), PlotOrientation.VERTICAL, false, false, false);
-		CategoryPlot xyPlot = (CategoryPlot) chart.getPlot();
-		NumberAxis range = (NumberAxis) xyPlot.getRangeAxis();
-		range.setStandardTickUnits(NumberAxis.createIntegerTickUnits());
-		assertEquals(data.buildLineChart(), chart);
+	public void iterationPieChart() {
+		assertEquals(ChartFactory.createPieChart3D("Requirements by Iteration",	data.toPieDataset(), true, false, false),
+				data.buildPieChart());
 	}
-	
 }
