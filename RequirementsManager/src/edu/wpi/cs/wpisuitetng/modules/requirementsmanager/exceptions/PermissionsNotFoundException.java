780e0ac069a546130942b819d9f50f51e4260791
Patrick Bobell <pbobell@wpi.edu>
2013-04-24 18:39:53 -0400

    Fixed authorship and added documentation for exceptions

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/exceptions/PermissionsNotFoundException.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/exceptions/PermissionsNotFoundException.java
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    @author Conor
+ *    @author Conor Geary
  *******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.exceptions;
