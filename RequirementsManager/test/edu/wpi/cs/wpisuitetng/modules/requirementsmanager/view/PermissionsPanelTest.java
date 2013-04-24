7d1dae22d1c4ef0938cd7d635186842aa88b431f
RiceX1055 <computers1055@gmail.com>
2013-04-24 19:45:50 -0400

    Added basic test for permissions panel

---
+++ RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/PermissionsPanelTest.java
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Copyright (c) 2013 -- WPI Suite: Team Swagasaurus
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Mitchell Caisse
+ *******************************************************************************/
+
+package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class PermissionsPanelTest {
+
+	/** the permissions panel to test */
+	private PermissionsPanel panel;
+	
+	
+	
+	@Before
+	public void setup() {
+		panel = null;
+	}
+	
+	/** Make sure that the constructor doesn't error or anything */
+	@Test
+	public void testConstructor() {
+		panel = new PermissionsPanel();
+		assertNotNull(panel);
+	}
+	
+	
+}
