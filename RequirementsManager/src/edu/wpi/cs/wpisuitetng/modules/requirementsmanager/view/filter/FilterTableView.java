bae2b90f20ba51426d9359964160adc255d9a5a4
RiceX1055 <computers1055@gmail.com>
2013-04-28 14:58:43 -0400

    Currently selected filter is unselected when cancel editing is pressed

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/filter/FilterTableView.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/filter/FilterTableView.java
@@ -38,78 +38,78 @@ import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.observers.notifiers.IS
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.tabs.MainTabController;
 
 public class FilterTableView extends JPanel implements
-		IRetrieveAllFiltersNotifier, ListSelectionListener, ISaveNotifier,
-		ActionListener {
-	
+IRetrieveAllFiltersNotifier, ListSelectionListener, ISaveNotifier,
+ActionListener {
+
 	/** List of all the filters this is displaying */
 	List<Filter> filters;
-	
+
 	/** the table view for the filters */
 	private final JTable tableView;
-	
+
 	/** The table model for the table view */
 	private final FilterTableModel tableModel;
-	
+
 	/** The scroll pane for the filter table */
 	private final JScrollPane scrollPane;
-	
+
 	/** Button used to enable or disable a filter */
 	private final JButton butEnable;
-	
+
 	/** Button used to delete a filter */
 	private final JButton butDelete;
-	
+
 	/** Panel to hold stuff in the scrollPane */
 	private final JPanel butPanel;
-	
+
 	/** The controller to retrieve filters */
 	private final FilterController filterController;
-	
+
 	/** The FilterView this view is contained in */
 	private final FilterView filterView;
-	
+
 	public FilterTableView(final FilterView filterView) {
 		this.filterView = filterView;
 		final List<Filter> filters = new ArrayList<Filter>();
 		filterController = new FilterController();
-		
+
 		butPanel = new JPanel();
-		
+
 		butEnable = new JButton("Disable");
 		butEnable.setEnabled(false);
 		butEnable.addActionListener(this);
-		
+
 		butDelete = new JButton("Delete");
 		butDelete.setEnabled(false);
 		butDelete.addActionListener(this);
-		
+
 		butPanel.add(butEnable);
 		butPanel.add(butDelete);
-		
+
 		tableModel = new FilterTableModel(filters);
-		
+
 		tableView = new FilterTable(tableModel);
 		tableView.setFillsViewportHeight(true);
 		tableView.getSelectionModel().addListSelectionListener(this);
-		
+
 		tableView.getColumnModel().removeColumn(
 				tableView.getColumnModel().getColumn(0));
-		
+
 		scrollPane = new JScrollPane(tableView,
 				ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
 				ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		
+
 		setLayout(new BorderLayout());
-		
+
 		add(scrollPane, BorderLayout.CENTER);
 		add(butPanel, BorderLayout.SOUTH);
-		
+
 		setPreferredSize(new Dimension(100, 500));
-		
+
 		refresh();
-		
+
 	}
-	
+
 	@Override
 	public void actionPerformed(final ActionEvent e) {
 		final Object source = e.getSource();
@@ -120,7 +120,7 @@ public class FilterTableView extends JPanel implements
 			} else {
 				active = false;
 			}
-			
+
 			// change the active status of the filters
 			final int[] selRows = tableView.getSelectedRows();
 			for (final int row : selRows) {
@@ -132,7 +132,7 @@ public class FilterTableView extends JPanel implements
 			}
 			// notify the listeners that we made changes
 			filterView.notifyListeners();
-			
+
 		} else if (source.equals(butDelete)) {
 			final int[] selRows = tableView.getSelectedRows();
 			for (final int row : selRows) {
@@ -145,31 +145,31 @@ public class FilterTableView extends JPanel implements
 			filterView.notifyListeners();
 		}
 	}
-	
+
 	@Override
 	public void fail(final Exception exception) {
 		System.out.println("Filter table View Exception!!");
 		exception.printStackTrace();
 	}
-	
+
 	public Requirement getDraggedRequirement() {
 		return null;
 	}
-	
+
 	public MainTabController getTabController() {
 		return null;
 	}
-	
+
 	@Override
 	public void receivedData(final Filter[] filters) {
 		updateFilters();
 	}
-	
+
 	/**
 	 * Refreshes the table view
 	 * 
 	 */
-	
+
 	public void refresh() {
 		// get the filters from the server
 		final RetrieveAllFiltersRequestObserver observer = new RetrieveAllFiltersRequestObserver(
@@ -177,34 +177,34 @@ public class FilterTableView extends JPanel implements
 		filterController.getAll(observer);
 		updateFilters();
 	}
-	
+
 	@Override
 	public void responseError(final int statusCode, final String statusMessage) {
 		System.out.println("Response error " + statusCode + " : "
 				+ statusMessage);
 	}
-	
+
 	@Override
 	public void responseSuccess() {
 		updateFilters();
 		filterView.notifyListeners();
 		tableView.repaint();
 	}
-	
+
 	private void updateFilters() {
 		final List<Filter> filters = FilterDatabase.getInstance().getAll();
 		tableModel.updateFilters(filters);
 	}
-	
+
 	@Override
 	public void valueChanged(final ListSelectionEvent e) {
-		updateButtonStatus();		
+		updateButtonStatus();
 	}
-	
-	/** Updates the status of the buttoms 
+
+	/** Updates the status of the buttoms
 	 * 
 	 */
-	
+
 	private void updateButtonStatus() {
 		if (tableView.getSelectedRowCount() == 0) {
 			butEnable.setEnabled(false);
@@ -213,7 +213,7 @@ public class FilterTableView extends JPanel implements
 		} else if (tableView.getSelectedRowCount() == 1) {
 			butEnable.setEnabled(true);
 			butDelete.setEnabled(true);
-			
+
 			final int selRow = tableView.getSelectedRow();
 			final Filter filter = tableModel.getFilterAt(selRow);
 			if (filter.isActive()) {
@@ -221,21 +221,21 @@ public class FilterTableView extends JPanel implements
 			} else {
 				butEnable.setText("Enable");
 			}
-			
+
 			filterView.editFilter(filter);
-			
+
 		} else {
 			// multiple filters are selected
 			butEnable.setEnabled(true);
 			butDelete.setEnabled(true);
 			filterView.cancelEdit();
-		}	
+		}
 	}
-	
+
 	/** Will disable all of the fields in this View
 	 * 
 	 */
-	
+
 	@Override
 	public void setEnabled(boolean enabled) {
 		super.setEnabled(enabled);
@@ -249,5 +249,9 @@ public class FilterTableView extends JPanel implements
 			butDelete.setEnabled(enabled);
 			butEnable.setEnabled(enabled);
 		}
+	}
+
+	public void clearSelection() {
+		tableView.clearSelection();
 	}
 }
