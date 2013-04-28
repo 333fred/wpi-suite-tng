7d7a0fc4bbe1453552e0fff17c26a124e7e6022c
RiceX1055 <computers1055@gmail.com>
2013-04-28 13:42:31 -0400

    Fixed the Requirement Changeset test

--- RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/models/RequirementChangesetTest.java
+++ RequirementsManager/test/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/models/RequirementChangesetTest.java
@@ -7,7 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    
+ * 
  *******************************************************************************/
 
 package edu.wpi.cs.wpisuitetng.modules.requirementsmanager.models;
@@ -21,16 +21,16 @@ import edu.wpi.cs.wpisuitetng.modules.core.models.User;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.models.logging.RequirementChangeset;
 
 public class RequirementChangesetTest {
-	
+
 	User user;
 	Session session;
-	
+
 	@Before
 	public void setup() {
 		user = new User("User's Name", "username", "swordfish", 1);
 		session = new Session(user, "SSID");
 	}
-	
+
 	@Test
 	public void testCreationLog() {
 		final Requirement r = new Requirement();
@@ -39,17 +39,17 @@ public class RequirementChangesetTest {
 		Assert.assertEquals(creationChangeset.getContent(),
 				"Created Requirement<br></html>");
 	}
-	
+
 	@Test
 	public void testEmptyContent() {
 		final RequirementChangeset rc = new RequirementChangeset();
 		Assert.assertEquals(rc.getContent(), "");
 	}
-	
+
 	@Test
 	public void testTitleRetrieval() {
 		final RequirementChangeset rc = new RequirementChangeset(user);
-		Assert.assertTrue(rc.getTitle().contains(("User's Name")));
+		Assert.assertTrue(rc.getTitle().contains(user.getUsername()));
 	}
-	
+
 }
