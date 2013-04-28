db3d0385c94c26b241624ee839a1fef6503d7561
Fredric Silberberg <github@fsilberberg.com>
2013-04-28 16:43:27 -0400

    Javadoc and warning updates for observers and view.actions

--- RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/actions/SaveEditingTableAction.java
+++ RequirementsManager/src/edu/wpi/cs/wpisuitetng/modules/requirementsmanager/view/actions/SaveEditingTableAction.java
@@ -16,20 +16,14 @@ import java.awt.event.ActionEvent;
 import javax.swing.AbstractAction;
 import javax.swing.table.TableModel;
 import javax.swing.table.TableRowSorter;
-import javax.swing.tree.DefaultMutableTreeNode;
 
-import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.commonenums.Type;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.commonenums.Priority;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.commonenums.Status;
-import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.controllers.IterationController;
+import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.commonenums.Type;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.controllers.RequirementsController;
-import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.exceptions.IterationNotFoundException;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.exceptions.RequirementNotFoundException;
-import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.localdatabase.IterationDatabase;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.localdatabase.RequirementDatabase;
-import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.models.Iteration;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.models.Requirement;
-import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.observers.UpdateIterationRequestObserver;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.observers.UpdateRequirementRequestObserver;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.observers.notifiers.ISaveNotifier;
 import edu.wpi.cs.wpisuitetng.modules.requirementsmanager.tabs.MainTabController;
@@ -65,7 +59,6 @@ public class SaveEditingTableAction extends AbstractAction implements
 	@Override
 	public void actionPerformed(final ActionEvent e) {
 		final RequirementsController saveController = new RequirementsController();
-		final IterationController iterationController = new IterationController();
 		final RequirementDatabase rdb = RequirementDatabase.getInstance();
 		
 		final boolean[] changedRows = tableView.getTable().getEditedRows();
@@ -82,93 +75,67 @@ public class SaveEditingTableAction extends AbstractAction implements
 				hasChanged = true;
 				final int id = Integer.parseInt((String) tableView.getTable()
 						.getModel().getValueAt(i, 0));
-				final String newName = (String) tableView.getTable()
-						.getModel().getValueAt(i, 1);
-				
-				String iterationName = (String)tableView.getTable().getModel().getValueAt(i, 5);				
-				Iteration newIteration = IterationDatabase.getInstance().getIteration(
-						iterationName);
+				final String newName = (String) tableView.getTable().getModel()
+						.getValueAt(i, 1);
 				
 				final int newEstimate = Integer.parseInt((String) tableView
 						.getTable().getModel().getValueAt(i, 6));
 				final int newEffort = Integer.parseInt((String) tableView
 						.getTable().getModel().getValueAt(i, 7));
 				final String newRelease = (String) tableView.getTable()
-						.getModel().getValueAt(i, 8);	
+						.getModel().getValueAt(i, 8);
 				try {
 					Requirement reqToChange = rdb.get(id);
 					UpdateRequirementRequestObserver observer = new UpdateRequirementRequestObserver(
 							this);
 					Requirement parentReq = null;
-					Iteration anIteration = null;
 					
 					reqToChange.setEstimate(newEstimate);
 					reqToChange.setName(newName);
 					reqToChange.setEffort(newEffort);
 					reqToChange.setReleaseNum(newRelease);
-										
 					
 					try {
-						reqToChange.setType(Type.valueOf(((String)tableView.getTable().getModel().getValueAt(i, 2)).toUpperCase().replaceAll(" ", "_").replaceAll("-", "_")));
+						reqToChange.setType(Type.valueOf(((String) tableView
+								.getTable().getModel().getValueAt(i, 2))
+								.toUpperCase().replaceAll(" ", "_")
+								.replaceAll("-", "_")));
 					} catch (final IllegalArgumentException except) {
-						//We use "" instead of "None"
+						// We use "" instead of "None"
 						reqToChange.setType(Type.BLANK);
 					}
 					
 					try {
-						reqToChange.setPriority(Priority.valueOf(((String)tableView.getTable().getModel().getValueAt(i, 3)).toUpperCase().replaceAll(" ", "_")));
+						reqToChange.setPriority(Priority
+								.valueOf(((String) tableView.getTable()
+										.getModel().getValueAt(i, 3))
+										.toUpperCase().replaceAll(" ", "_")));
 					} catch (final IllegalArgumentException except) {
 						reqToChange.setPriority(Priority.BLANK);
 					}
 					
 					try {
-					reqToChange.setStatus(Status.valueOf(((String)tableView.getTable().getModel().getValueAt(i, 4)).toUpperCase().replaceAll(" ", "_")));
+						reqToChange.setStatus(Status
+								.valueOf(((String) tableView.getTable()
+										.getModel().getValueAt(i, 4))
+										.toUpperCase().replaceAll(" ", "_")));
 					} catch (final IllegalArgumentException except) {
 						reqToChange.setStatus(Status.BLANK);
 					}
 					
-					
-					if(reqToChange.getStatus().equals(Status.DELETED)&&reqToChange.getpUID().size()>0){
-						parentReq = RequirementDatabase.getInstance().get(reqToChange.getpUID().get(0));
+					if (reqToChange.getStatus().equals(Status.DELETED)
+							&& reqToChange.getpUID().size() > 0) {
+						parentReq = RequirementDatabase.getInstance().get(
+								reqToChange.getpUID().get(0));
 						parentReq.removeSubRequirement(reqToChange.getrUID());
 						reqToChange.removePUID(parentReq.getrUID());
 						saveController.save(parentReq, observer);
 					}
 					
-					if(reqToChange.getStatus().equals(Status.DELETED)){
+					if (reqToChange.getStatus().equals(Status.DELETED)) {
 						reqToChange.setIteration(-2);
-						/*try {
-							anIteration = IterationDatabase.getInstance().get(reqToChange.getIteration());
-							anIteration.removeRequirement(reqToChange.getrUID());
-							final UpdateIterationRequestObserver observerDelete = new UpdateIterationRequestObserver(
-									this);
-							iterationController.save(anIteration, observerDelete);
-						} catch (IterationNotFoundException e1) {
-							e1.printStackTrace();
-						}
-						
-						anIteration = IterationDatabase.getInstance().getIteration("Deleted");						
-						reqToChange.setIteration(anIteration.getId());
-						anIteration.addRequirement(reqToChange.getrUID());
-						final UpdateIterationRequestObserver observer3 = new UpdateIterationRequestObserver(this);
-						iterationController.save(anIteration, observer3);*/
-					} else if(reqToChange.getStatus().equals(Status.OPEN)){
+					} else if (reqToChange.getStatus().equals(Status.OPEN)) {
 						reqToChange.setIteration(-1);
-						/*try {
-							anIteration = IterationDatabase.getInstance().get(reqToChange.getIteration());
-							anIteration.removeRequirement(reqToChange.getrUID());
-							final UpdateIterationRequestObserver observerDelete = new UpdateIterationRequestObserver(
-									this);
-							iterationController.save(anIteration, observerDelete);
-						} catch (IterationNotFoundException e1) {
-							e1.printStackTrace();
-						}
-						
-						anIteration = IterationDatabase.getInstance().getIteration("Backlog");						
-						reqToChange.setIteration(anIteration.getId());
-						anIteration.addRequirement(reqToChange.getrUID());
-						final UpdateIterationRequestObserver observer3 = new UpdateIterationRequestObserver(this);
-						iterationController.save(anIteration, observer3);*/
 					}
 					
 					saveController.save(reqToChange, observer);
@@ -183,7 +150,7 @@ public class SaveEditingTableAction extends AbstractAction implements
 			tableView.displayEditInformation("");
 			tableView.changeButtonStatus();
 			tableView.getTable().clearUpdated();
-			if(!hasChanged) {
+			if (!hasChanged) {
 				tableView.refresh();
 			}
 		}
